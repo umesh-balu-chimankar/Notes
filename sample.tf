2f736aa79bc604e17724a1c7e96fc255c03bcdd8
Umesh <umeshchimankar03@gmail.com>
2025-12-28 10:57:57 +0530

    initial instance creation

--- sample.tf
+++ sample.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "myinstance" {
     ami = "ami-087d1c9a513324697"
     instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-038c233d26eb54582"]
+    vpc_security_group_ids = ["sg-000d393420ab317b8"]
     key_name = "id_rsa_key"
     availability_zone = "ap-south-1b"
   tags = {
