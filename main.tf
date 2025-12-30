f3b33baa12c9904419aab73d0143c4aad2b93791
Umesh <umeshchimankar03@gmail.com>
2025-12-30 10:14:44 +0530

    separate file added

--- main.tf
+++ main.tf
@@ -8,20 +8,20 @@ terraform {
     key = "tfstate"
   }
 }
-data "aws_security_group" "mysg" {
+data "aws_security_group" "my-sg" {
   filter {
     name   = "vpc-id"
     values =[var.vpc_id]
   }
   filter {
     name   = "group-name"
-    values = ["mysg"]
+    values = ["my-sg"]
   }
 }
 resource "aws_instance" "myinstance" {
     ami = var.ami_id
     instance_type = var.instance_type
-   vpc_security_group_ids = [data.aws_security_group.mysg.id]
+   vpc_security_group_ids = [data.aws_security_group.my-sg.id]
     key_name = var.key_name
     availability_zone = var.availability_zone
     tags = {
