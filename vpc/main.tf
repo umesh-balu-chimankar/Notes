5dceb91f54c6d0bece82f39f80aa052a975f4772
Umesh <umeshchimankar03@gmail.com>
2025-12-30 11:51:43 +0530

    vpc file added

---
+++ vpc/main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block           = "10.0.0.0/16"
+
+  tags = {
+    Name = "prod-vpc"
+  }
+}
+resource "aws_subnet" "pvtsubnet" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/20"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+resource "aws_subnet" "pubsubnet" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.2.0/20"
+  availability_zone       = "ap-south-1b" 
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "prod-igw"
+  }
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.pubsubnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+
+
